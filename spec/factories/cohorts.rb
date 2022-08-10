991f16c26f5f565e2e1775cfe21092aa5e4430dd
Sarah Proctor <sproctor950@gmail.com>
2022-08-09 19:57:16 -0700

    :card_file_box: adds cohort scaffold build

---
+++ spec/factories/cohorts.rb
@@ -0,0 +1,5 @@
+FactoryBot.define do
+  factory :cohort do
+    cohort_name { "MyString" }
+  end
+end
---
+++ spec/views/cohorts/edit.html.erb_spec.rb
@@ -0,0 +1,18 @@
+require 'rails_helper'
+
+RSpec.describe "cohorts/edit", type: :view do
+  before(:each) do
+    @cohort = assign(:cohort, Cohort.create!(
+      cohort_name: "MyString"
+    ))
+  end
+
+  it "renders the edit cohort form" do
+    render
+
+    assert_select "form[action=?][method=?]", cohort_path(@cohort), "post" do
+
+      assert_select "input[name=?]", "cohort[cohort_name]"
+    end
+  end
+end
