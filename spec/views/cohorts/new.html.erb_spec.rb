4386a4f234045bf34c9984acd6b7d45f4c981396
Renzlawr <RSLogan23@gmail.com>
2022-08-12 11:12:37 -0600

    ran linter and added ruby setup github action

--- spec/views/cohorts/new.html.erb_spec.rb
+++ spec/views/cohorts/new.html.erb_spec.rb
@@ -1,4 +1,4 @@
-require 'rails_helper'
+require "rails_helper"
 
 RSpec.describe "cohorts/new", type: :view do
   before(:each) do
@@ -11,7 +11,6 @@ RSpec.describe "cohorts/new", type: :view do
     render
 
     assert_select "form[action=?][method=?]", cohorts_path, "post" do
-
       assert_select "input[name=?]", "cohort[cohort_name]"
     end
   end
