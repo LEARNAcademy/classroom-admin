b0a3dafa7238bcd90c3e6b86ffce90508e79af06
Renzlawr <RSLogan23@gmail.com>
2022-09-27 12:05:10 -0600

    General styling updated and pages protected

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -14,6 +14,7 @@ class ApplicationController < ActionController::Base
   include Authorization
 
   before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :authenticate_user!
 
   impersonates :user
 
