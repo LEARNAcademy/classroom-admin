f5df8a1a1f8c267dc2dace155f4d26f7194ffb57
Genemichael25 <genemichael25@yahoo.com>
2022-12-16 14:09:16 -0700

    Lint issues resolved

--- config/application.rb
+++ config/application.rb
@@ -33,12 +33,12 @@ module Classroom
     # end
 
     # This also configures session_options for use below
-     config.session_store :cookie_store, key: '_interslice_session'
+    config.session_store :cookie_store, key: "_interslice_session"
 
     # Required for all session management (regardless of session_store)
-      config.middleware.use ActionDispatch::Cookies
- 
-      config.middleware.use config.session_store, config.session_options
+    config.middleware.use ActionDispatch::Cookies
+
+    config.middleware.use config.session_store, config.session_options
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
 
