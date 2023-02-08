8d38f1c984cbd3990bd66513d4bab9767b545a4d
Shadow090509 <jamoso090509@gmail.com>
2023-02-08 15:13:21 -0800

    :memo: [97] Rails Update README

--- app/helpers/sort_helper.rb
+++ app/helpers/sort_helper.rb
@@ -1,12 +1,12 @@
 module SortHelper
   def sortable(relation, column, title, options = {})
     matching_column = column.to_s == params[:sort]
-    direction = sort_direction == "asc" ? "desc" : "asc"
+    direction = (sort_direction == "asc") ? "desc" : "asc"
 
     link_to request.params.merge(sort: column, direction: direction), options do
       concat title
       if matching_column
-        caret = sort_direction == "asc" ? "up" : "down"
+        caret = (sort_direction == "asc") ? "up" : "down"
         concat " "
         concat tag.i(nil, class: "fas fa-caret-#{caret}")
       end
